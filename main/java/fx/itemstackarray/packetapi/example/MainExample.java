ddef6c42f9c772c433d69592eb20bc0d28e9fc93
extsa <extsayzz@gmail.com>
2023-04-22 13:41:33 +0200

    [+] Added splitter and decompress pipeline to inject [+] Cleaned up code

--- main/java/fx/itemstackarray/packetapi/example/MainExample.java
+++ main/java/fx/itemstackarray/packetapi/example/MainExample.java
@@ -7,11 +7,13 @@ public final class MainExample /* extends JavaPlugin*/ {
     private static PacketInjector packetInjector;
 
     public void onEnable() {
+
         /* Set the decoder name from your AntiCrash */
         packetInjector.setDecoder_name("your-decoder-name");
         /* Set the splitter name from your AntiCrash */
         packetInjector.setSplitter_name("your-splitter-name");
         /* Set the decompress name from your AntiCrash */
         packetInjector.setDecompress_name("your-decompress-name");
+
     }
 }
